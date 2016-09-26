c28f42dcf80e52b54fe97fa27933d0e9fed58580
XIjie <Xijie1@users.noreply.github.com>
2016-09-26 02:29:05 -0600

    Add files via upload

---
+++ HW_A4/README.md
@@ -0,0 +1,3 @@
+Blog for HW 3.1
+
+This i finish homework A 3.1.
---
+++ HW_A4/sketch.js
@@ -0,0 +1,82 @@
+var spot = {
+   x: 200,
+   y: 100
+ };
+
+
+  var col = {
+    r:255,
+    g:0,
+    b:0
+  };
+  
+  var speed = 10;
+  
+function setup() {
+  createCanvas(1000,1000);
+
+  colorMode(RGB);
+  
+  background(0);
+  
+  frameRate(30);
+ 
+
+
+  
+}
+
+function draw() {
+differentcolor();
+shapes();
+
+  
+
+function differentcolor() {
+  
+   col.r = random(100, 255);
+  col.g= random(0) ;
+  col.b= random(50, 180);
+  
+    }
+ 
+  
+  
+   
+   
+    
+function shapes (){
+  
+ fill(col.r, col.g, col.b);
+  if ( mouseX >30 && mouseY <500){
+  rect(spot.x, spot.y, 500, 500);
+  }
+  var x=0, y=0;
+  fill(col.r, col.g, col.b);
+   if (spot.x > width ){
+    speed = -3;
+    
+    
+
+  }
+
+  
+  spot.x =  spot.x + speed;
+  
+
+ 
+ for (var x = 0; x < width; x = x+50){
+  for(var y = 0; y <= height;y++)
+ ellipse(x, y*100, 20, 20);
+  
+  }
+
+x++
+
+
+
+   }
+   
+   if (mouseIsPressed){
+    background(100,255,100);}
+}
